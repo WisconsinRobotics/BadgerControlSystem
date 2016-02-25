bdadaf17998cc81cef80ab2e215bd893a49edcae
Ziliang Guo <zguo2@wisc.edu>
2016-02-24 23:46:19 -0600

    Add missing files.

---
+++ BadgerControlModule/Services/BadgerControlService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BadgerJaus.Messages;
+using BadgerJaus.Messages.Control;
+using BadgerJaus.Services;
+using BadgerJaus.Util;
+
+using BadgerControlModule.Models;
+
+namespace BadgerControlModule.Services
+{
+    class BadgerControlService : BaseService
+    {
+        BadgerControlSubsystem badgerControlSubsystem;
+        ConfirmControl confirmControlMessage;
+        RejectControl rejectControlMessage;
+
+        public BadgerControlService(BadgerControlSubsystem badgerControlSubsystem)
+        {
+            this.badgerControlSubsystem = badgerControlSubsystem;
+
+            confirmControlMessage = new ConfirmControl();
+            rejectControlMessage = new RejectControl();
+        }
+
+        public override bool ImplementsAndHandledMessage(Message message, Component component)
+        {
+            Subsystem remoteSubsystem;
+            Node remoteNode;
+            Component remoteComponent;
+
+            switch (message.GetCommandCode())
+            {
+                case JausCommandCode.CONFIRM_CONTROL:
+                    confirmControlMessage.SetFromJausMessage(message);
+                    if(confirmControlMessage.GetResponseCode() == ConfirmControl.CONTROL_ACCEPTED)
+                    {
+                        if (!badgerControlSubsystem.DiscoveredSubsystemsDictionary.TryGetValue(confirmControlMessage.GetSource().SubsystemID, out remoteSubsystem))
+                            return true;
+
+                        if (!remoteSubsystem.NodeDictionary.TryGetValue(confirmControlMessage.GetSource().getNode(), out remoteNode))
+                            return true;
+
+                        if (!remoteNode.ComponentDictionary.TryGetValue(confirmControlMessage.GetSource().getComponent(), out remoteComponent))
+                            return true;
+
+                        remoteComponent.ControlState = ControlState.STATE_CONTROLLED;
+                    }
+
+                    return true;
+
+                case JausCommandCode.REJECT_CONTROL:
+                    rejectControlMessage.SetFromJausMessage(message);
+                    if(rejectControlMessage.GetResponseCode() == RejectControl.CONTROL_RELEASED)
+                    {
+                        if (!badgerControlSubsystem.DiscoveredSubsystemsDictionary.TryGetValue(confirmControlMessage.GetSource().SubsystemID, out remoteSubsystem))
+                            return true;
+
+                        if (!remoteSubsystem.NodeDictionary.TryGetValue(confirmControlMessage.GetSource().getNode(), out remoteNode))
+                            return true;
+
+                        if (!remoteNode.ComponentDictionary.TryGetValue(confirmControlMessage.GetSource().getComponent(), out remoteComponent))
+                            return true;
+
+                        remoteComponent.ControlState = ControlState.STATE_NOT_CONTROLLED;
+                    }
+                    return true;
+
+                default: return false;
+            }
+        }
+
+        public override bool IsSupported(int commandCode)
+        {
+            switch (commandCode)
+            {
+                case JausCommandCode.CONFIRM_CONTROL:
+                case JausCommandCode.REJECT_CONTROL:
+                    return true;
+                default: return false;
+            }
+        }
+    }
+}
---
+++ BadgerControlModule/Services/BadgerDriverService.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Prism.Events;
+
+using BadgerJaus.Messages;
+using BadgerJaus.Services;
+using BadgerJaus.Util;
+
+using JoystickLibrary;
+
+using BadgerControlModule.Models;
+using BadgerControlModule.Views;
+
+namespace BadgerControlModule.Services
+{
+    class BadgerDriverService : BaseService
+    {
+        protected readonly IEventAggregator _eventAggregator;
+
+        JoystickQueryThread joystickQuery;
+
+        long prevXVelocity;
+        long prevYVelocity;
+
+        bool joystickConfirmedFromVisualView;
+        bool joystickConfirmedFromStatusView;
+        bool joystickMessageGiven;
+
+        BadgerControlSubsystem badgerControlSubsystem;
+
+        public BadgerDriverService(BadgerControlSubsystem badgerControlSubsystem)
+        {
+            this.badgerControlSubsystem = badgerControlSubsystem;
+
+            joystickConfirmedFromVisualView = false;
+            joystickConfirmedFromStatusView = false;
+            joystickMessageGiven = false;
+
+            _eventAggregator = ApplicationService.Instance.EventAggregator;
+
+            _eventAggregator.GetEvent<ConfirmJoystickEvent>().Subscribe((confirmationID) =>
+            {
+                if (confirmationID == BadgerControlStatusView.JOYSTICK_ID)
+                    joystickConfirmedFromStatusView = true;
+                else if (confirmationID == BadgerControlVisualView.JOYSTICK_ID)
+                    joystickConfirmedFromVisualView = true;
+
+                if (JoyStickConfirmed && !joystickMessageGiven)
+                {
+                    _eventAggregator.GetEvent<LoggerEvent>().Publish("Joystick connected.");
+                    joystickMessageGiven = true; // jank hack to fix the duplicate event publishing due to multithreading
+                }
+            });
+        }
+
+        public override bool ImplementsAndHandledMessage(Message message, Component component)
+        {
+            return false;
+        }
+
+        public override bool IsSupported(int commandCode)
+        {
+            return false;
+        }
+
+        protected override void Execute(Component component)
+        {
+            long xVelocity, yVelocity, zRotation;
+            int leftSpeed, rightSpeed;
+            int speedIndex;
+
+            if (joystickQuery == null)
+            {
+                joystickQuery = new JoystickQueryThread();
+                Thread joystickThread = new Thread(joystickQuery.QueryJoystick);
+                joystickThread.Start();
+            }
+
+            xVelocity = joystickQuery.XVelocity;
+            yVelocity = joystickQuery.YVelocity;
+            zRotation = joystickQuery.ZRotation;
+
+            if ((prevXVelocity == xVelocity) && (prevYVelocity == yVelocity))
+                return;
+
+            prevXVelocity = xVelocity;
+            prevYVelocity = yVelocity;
+
+            if (badgerControlSubsystem.CurrentDriveMode != null)
+                badgerControlSubsystem.CurrentDriveMode.SendDriveCommand(xVelocity, yVelocity, zRotation);
+        }
+
+        public override long SleepTime
+        {
+            get
+            {
+                return 100;
+            }
+        }
+
+        private bool JoyStickConfirmed
+        {
+            get
+            {
+                if (joystickConfirmedFromStatusView && joystickConfirmedFromVisualView)
+                    return true;
+                return false;
+            }
+        }
+    }
+}
