b9f80ec1294ccc0bb2c72c310da6e7f051092eda
Connor Quagliana <cquagliana@wisc.edu>
2016-05-23 20:03:19 -0500

    Added map view basics.

---
+++ BadgerControlModule/ViewModels/BadgerControlMapViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+using System.Net;
+
+using Prism.Mvvm;
+using Prism.Commands;
+using Prism.Events;
+
+using BadgerJaus.Util;
+
+using BadgerControlModule.Models;
+using BadgerControlModule.Services;
+
+namespace BadgerControlModule.ViewModels
+{
+    class BadgerControlMapViewModel : BindableBase
+    {
+        protected readonly IEventAggregator _eventAggregator;
+        private BadgerControlSubsystem badgerControlSubsystem;
+
+        public BadgerControlMapViewModel(IEventAggregator eventAggregator)
+        {
+            this._eventAggregator = eventAggregator;
+            badgerControlSubsystem = BadgerControlSubsystem.GetInstance();
+        }
+    }
+}
